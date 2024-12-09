c0e6086f3d7932e983425383b212d563ade2fc0d
Dzmitry Kalianchuk <koirn.dev@gmail.com>
2024-12-09 23:39:03 +0100

    Day 9 Puzzle 2 done

--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -11,6 +11,12 @@ fun main() {
         calculateCheckSum(moveBlocks(unpackDiskMap(input)))
     }
     println("Puzzle 1 output: $output. Done in ${duration.inWholeMilliseconds} ms" )
+
+    println("Puzzle 2 started")
+    val (output2, duration2) = measureTimedValue {
+        puzzle2(input)
+    }
+    println("Puzzle 2 output: $output2. Done in ${duration2.inWholeMilliseconds} ms" )
 }
 
 fun unpackDiskMap(input: String): List<Int> {
@@ -53,4 +59,74 @@ fun moveBlocks(input: List<Int>): List<Int> {
 fun calculateCheckSum(input: List<Int>): Long {
     return input.map { if (it > 0) it else 0 }
         .mapIndexed { index, i -> index * i.toLong() }.sum()
+}
+
+data class Block(val id: Int = EMPTY_ID, val size: Int = 0) {
+    companion object {
+        const val EMPTY_ID = -1
+    }
+}
+
+fun parseDiskMap(input: String): List<Block> {
+    return input.foldIndexed(mutableListOf()) { index, blockMap, c ->
+        val value = c.toString().toInt()
+        val id = if (index % 2 == 0) {
+            index / 2
+        } else {
+            Block.EMPTY_ID
+        }
+        blockMap.add(Block(id = id, size = value))
+        blockMap
+    }
+}
+
+fun moveFiles(input: List<Block>): List<Block> {
+    val list = input.toMutableList()
+
+    var right = input.lastIndex
+
+    while (right > 0) {
+        val block = list[right]
+
+        if (block.id != Block.EMPTY_ID) {
+            val left = list.indexOfFirst { it.id == Block.EMPTY_ID && it.size >= block.size}
+            if (left in 1..<right) {
+                val empty = list[left]
+
+                if (empty.size > block.size) {
+                    list[left] = block
+                    list[right] = Block(id = Block.EMPTY_ID, size = block.size)
+                    list.add(left + 1, Block(id = Block.EMPTY_ID, size = empty.size - block.size))
+                    right++
+                } else {
+                    list[left] = block
+                    list[right] = Block(id = Block.EMPTY_ID, size = block.size)
+                }
+            }
+        }
+        right--
+    }
+
+    return list
+}
+
+fun unpackBlockMap(input: List<Block>): List<Int> {
+    return buildList {
+        input.forEach { block ->
+            repeat(block.size) {
+                add(block.id)
+            }
+        }
+    }
+}
+
+fun puzzle2(input: String): Long {
+    val blockMap = parseDiskMap(input)
+    val newBlockMap = moveFiles(blockMap)
+    val unpackedMap = unpackBlockMap(newBlockMap)
+    return calculateCheckSum(unpackedMap)
+}
+
+fun diskMapToString(input: List<Int>): String {
+    return input.joinToString("") { if (it > -1 ) { it.toString() } else "." }
 }
--- src/test/kotlin/Day9Tests.kt
+++ src/test/kotlin/Day9Tests.kt
@@ -1,6 +1,4 @@
-import adventofcode2023.day9.calculateCheckSum
-import adventofcode2023.day9.moveBlocks
-import adventofcode2023.day9.unpackDiskMap
+import adventofcode2023.day9.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
@@ -32,6 +30,59 @@ class Day9Tests {
         assertThat(result).isEqualTo(1928L)
     }
 
+    @ParameterizedTest
+    @MethodSource("parseDiskSourceProvider")
+    fun testParseDisk(testCase: Pair<String, List<Block>>) {
+        val result = parseDiskMap(testCase.first)
+
+        assertThat(result).containsExactlyElementsOf(testCase.second)
+    }
+
+    @ParameterizedTest
+    @MethodSource("unpackSourceProvider")
+    fun testUnpackBlockMap(testCase: Pair<String, List<Int>>) {
+        val result = unpackBlockMap(parseDiskMap(testCase.first))
+
+        assertThat(result).containsExactlyElementsOf(testCase.second)
+    }
+
+    @Test
+    fun testMoveFiles() {
+        // 2333133121414131402
+//        val input = listOf(Block(0, 2), Block(-1, 3), Block(1, 3), Block(-1, 3),
+//            Block(2,1), Block(-1, 3), Block(3, 3), Block(-1, 1), Block(4,2), Block(-1,1),
+//            Block(5, 4), Block(-1, 1), Block(6, 4), Block(-1,1), Block(7,3), Block(-1,1),
+//            Block(8,4), Block(-1, 0), Block(9,2))
+
+        // 00992111777.44.333....5555.6666.....8888..
+//        val expected = listOf(Block(0, 2),Block(9, 2), Block(2,1), Block(1, 3), Block(7, 3), Block(-1, 1),
+//            Block(4, 2), Block(-1, 1), Block(3, 3), Block(-1,4), Block(5,4), Block(-1,1),
+//            Block(5,4), Block(-1,5), Block(8,4), Block(-1,2))
+
+        val input = "2333133121414131402"
+        val blockMap = parseDiskMap(input)
+        val unpackedMap = unpackBlockMap(blockMap)
+
+        assertThat(diskMapToString(unpackedMap)).isEqualTo("00...111...2...333.44.5555.6666.777.888899")
+
+        val actual = moveFiles(blockMap)
+
+        val unpackedMapActual = unpackBlockMap(actual)
+
+        assertThat(diskMapToString(unpackedMapActual)).isEqualTo("00992111777.44.333....5555.6666.....8888..")
+
+//        assertThat(actual).containsExactlyElementsOf(expected)
+    }
+
+    @Test
+    fun testPuzzle2() {
+        val input = "2333133121414131402"
+
+        val result = puzzle2(input)
+
+        assertThat(result).isEqualTo(2858)
+    }
+
     companion object {
 
         @JvmStatic
@@ -45,6 +96,15 @@ class Day9Tests {
             listOf(0,-1,-1,1,1,1,-1,-1,-1,-1,2,2,2,2,2) to listOf(0,2,2,1,1,1,2,2,2,-1,-1,-1,-1,-1,-1),
             listOf(0,0,-1,-1,-1,1,1,1,-1,-1,-1,2,-1,-1,-1,3,3,3,-1,4,4,-1,5,5,5,5,-1,6,6,6,6,-1,7,7,7,-1,8,8,8,8,9,9) to
             listOf(0,0,9,9,8,1,1,1,8,8,8,2,7,7,7,3,3,3,6,4,4,6,5,5,5,5,6,6,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1),
+        )
+
+        @JvmStatic
+        fun parseDiskSourceProvider():Stream<Pair<String, List<Block>>> = Stream.of(
+            "12345" to listOf(Block(0,1), Block(-1,2), Block(1,3), Block(-1,4), Block(2,5)),
+            "2333133121414131402" to listOf(Block(0, 2), Block(-1, 3), Block(1, 3), Block(-1, 3),
+                Block(2,1), Block(-1, 3), Block(3, 3), Block(-1, 1), Block(4,2), Block(-1,1),
+                Block(5, 4), Block(-1, 1), Block(6, 4), Block(-1,1), Block(7,3), Block(-1,1),
+                Block(8,4), Block(-1, 0), Block(9,2))
         )
     }
 }
